1189fdd28647a6c1b9d67336ccea8fb14b7fcfec
Satria Bagus <satria.bagus18@gmail.com>
2019-10-10 06:53:55 +0700

    Menambahkan fitur pada Home/Index

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,3 +1,11 @@
 
-    <h1>Home/Index</h1>
-
+<br>
+    <div class="container">
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, <?=$nama;?></h1>
+        <p class="lead">Rekayasa Web, Pembahasan CodeIgniter 3.x</p>
+        <hr class="my-4">
+        <p>Halaman Home/Index CodeIgniter</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+    </div>
