4cb523462becead714652945a93fbe6abdc07f15
Satria Bagus <satria.bagus18@gmail.com>
2019-10-10 06:41:12 +0700

    Mengirimkan array judul ke halaman

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,6 +2,9 @@
 
 class Home extends CI_Controller{
     public function index(){
+        $data['judul'] = 'Halaman Home';
+        $this->load->view('templates/header', $data);
         $this->load->view('home/index');
+        $this->load->view('templates/footer');
     }
 }
