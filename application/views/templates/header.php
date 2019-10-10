75aa829fc9da97d24bd64a8282fef5789a419ab8
Satria Bagus <satria.bagus18@gmail.com>
2019-10-10 14:17:23 +0700

    Memperbaiki link pada navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,15 +14,15 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">CI App</a>
+            <a class="navbar-brand" href="<?= base_url();?>">CI App</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">Mahasiswa</a>
-                <a class="nav-item nav-link" href="#">About</a>
+                <a class="nav-item nav-link" href="<?= base_url();?>">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="<?= base_url();?>mahasiswa">Mahasiswa</a>
+                <a class="nav-item nav-link" href="<?= base_url();?>about">About</a>
             </div>
         </div>
         </div>
